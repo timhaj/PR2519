01f0254c08cfc3813ae2d88e30524999bd1944b8
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2025-05-07 19:47:46 +0200

    Average line now on nearest 15min mark

--- prvi_primer.py
+++ prvi_primer.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 
 df = pd.read_csv("./podatki/parking_data.csv")
+df2 = pd.read_csv("./podatki/parking_data2.csv")
+df = pd.concat([df, df2])
 df = df[(df["Prosto"] != "/") & (~df["Prosto"].isna()) & (df["Location"] != "Slovenčeva ulica")]
 
 base_date = pd.to_datetime("1970-01-01")
-p = df[df["Location"] == "PH Kongresni trg"].copy()
+p = df[df["Location"] == "Gospodarsko razstavišče"].copy()
 p["Datum"] = pd.to_datetime(p["Datum"])
 plt.figure(figsize=(15, 5))
 
 all_days_data = []
-for day in pd.date_range(start="2025-03-18", end="2025-04-10"):
+for day in pd.date_range(start="2025-03-18", end="2025-04-15"):
     day_data = p[(p["Datum"] >= day) & (p["Datum"] < day + pd.Timedelta(days=1))].copy()
 
     if str(day) in ["2025-03-29 00:00:00", "2025-04-05 00:00:00", "2025-04-07 00:00:00", "2025-04-08 00:00:00", "2025-04-09 00:00:00"]:
@@ -29,9 +32,12 @@ for day in pd.date_range(start="2025-03-18", end="2025-04-10"):
 
 
 combined = pd.concat(all_days_data)
-avg_data = combined.groupby("Time").mean().reset_index()
-avg_data["Smoothed"] = avg_data["Normalized"].rolling(window=10, min_periods=1).mean()
-plt.plot(avg_data["Time"], avg_data["Smoothed"], color="black", linewidth=3, label="Povprečje")
+combined['To15min'] = combined['Time'].dt.round('15min').dt.time.apply(lambda t: datetime.datetime.combine(datetime.datetime(1970, 1, 1), t))
+combined = combined[["To15min", "Normalized"]]
+avg_data = combined.groupby("To15min").mean().reset_index()
+#avg_data = combined.groupby("Time").mean().reset_index()
+#avg_data["Smoothed"] = avg_data["Normalized"].rolling(window=10, min_periods=1).mean()
+plt.plot(avg_data["To15min"], avg_data["Normalized"], color="black", linewidth=3, label="Povprečje")
 plt.ylim(-50, 105)
 tick_locs = pd.date_range(start=base_date, periods=25, freq="1H")
 tick_labels = tick_locs.strftime('%H:%M')
@@ -43,5 +49,3 @@ plt.title('Dnevna primerjava zasedenosti parkirišča "PH Kongresni trg" (24h)')
 plt.legend(ncol=3, fontsize=8)
 plt.tight_layout()
 plt.show()
-
-y = 0
